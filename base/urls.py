a4e491a236e9ca53860481f8f9e44e9db50056c6
nikunjhadiya <ahirnikunj1122@gmail.com>
2026-06-16 17:08:07 +0530

    Completed Trash and Restore Features

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
-from .views import home
+from .views import *
 
 urlpatterns = [
     path('', home, name='home'),
+    path('trash/',trash,name='trash'),
+    path('delete/<int:id>/',delete,name='delete'),
+    path('trestore/<int:pk>/',trestore,name='trestore'),
+    path('deleteall/',deleteall,name = 'deleteall'),
+    path('trestoreall/',trestoreall,name = 'trestoreall'),
+    path('tdeleteall/', tdeleteall, name='tdeleteall'),
+    path('tdelete/<int:pk>/', tdelete, name='tdelete'),
+
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import TaskModel
 
 def home(request):
-    return render(request, "home.html")
+    data = TaskModel.objects.filter(host = request.user,is_delete = False)
+    if request.method =='POST':
+        TaskModel.objects.create(
+        title = request.POST['title'],
+        desc = request.POST['desc'],
+        host = request.user
+        )
+    return render(request,'home.html',{'data':data})
+
+def trash(request):
+    data = TaskModel.objects.filter(host = request.user,is_delete = True)
+    return render(request,'trash.html',{'data':data})
+
+def delete(request,id):
+    data = TaskModel.objects.get(id=id)
+    data.is_delete = True
+    data.save()
+    return redirect('home')
+
+def trestore(request,pk):
+    data = TaskModel.objects.get(id=pk)
+    data.is_delete = False
+    data.save()
+    return redirect(home)
+
+def deleteall(request):
+    data = TaskModel.objects.filter(host = request.user,is_delete = False)
+    for i in data:
+        i.is_delete = True
+        i.save()
+    return redirect('trash')    
+
+def trestoreall(request):
+    data = TaskModel.objects.filter(host = request.user,is_delete = True)
+    for i in data:
+        i.is_delete = False
+        i.save()
+    return redirect('home') 
+
+# permanently delete data
+def tdeleteall(request):
+    data = TaskModel.objects.filter(host=request.user,is_delete=True)
+    data.delete()
+    return redirect('trash')
+
+# permanently delete only that task
+def tdelete(request, pk):
+    data = TaskModel.objects.get(id=pk)
+    data.delete()
+    return redirect('trash')
